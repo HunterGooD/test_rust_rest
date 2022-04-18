4859321744018e5b3828e9ec994044d8042a1e8c
Vlad Gudov <vlad.gudov@inbox.ru>
2022-04-18 21:40:18 +0400

    cargo fmt; time check on load image

--- src/internal/image/mod.rs
+++ src/internal/image/mod.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashMap, vec, io::Cursor};
+use std::time::Instant;
+use std::{collections::HashMap, vec};
 
-use rocket::tokio::{self, fs::{self, File}, sync::Mutex, io::{AsyncReadExt, AsyncWriteExt}};
+use rocket::tokio::{self, fs::File, io::AsyncReadExt, sync::Mutex};
 
 pub mod facedetect;
 
@@ -9,32 +10,33 @@ pub async fn process_image(
     file_name: &str,
     path: &str,
 ) -> Result<(), image::ImageError> {
+    let now = Instant::now();
+
     println!("Path: {}, name: {}", path, file_name);
 
     let mut _file = match File::open(path).await {
         Err(why) => panic!("couldn't open {}: {}", path, why),
         Ok(file) => file,
     };
-    let mut buf = vec![];
 
+    let mut buf: Vec<u8> = vec![];
     _file.read_to_end(&mut buf).await.expect("Error read file");
 
+    let d_image = image::load_from_memory(&buf).expect("error load buf to image");
+
     let sv_path = String::from("images/") + (file_name);
-    println!("{}", sv_path.to_lowercase());
-    let mut _file = File::create(sv_path.to_lowercase()).await.unwrap();
-    _file.write_all_buf(&mut Cursor::new(&buf)).await.expect("Error write buf to file");
-    image::load_from_memory(&buf);
-        // let file_image = match image::open(path) {
-    //     Ok(f_image) => f_image,
-    //     Err(e) => return Err(e),
-    // };
-    // let sv_path = String::from("/images/") + (file_name);
-    // match file_image.save(sv_path) {
-    //     Ok(_) => {}
-    //     Err(e) => return Err(e),
-    // };
-    // let mut h_m = m.lock().await;
-    // h_m.insert(file_name.to_string(), String::from("sucess"));
-    
+    d_image
+        .save(sv_path.to_lowercase())
+        .expect("Error save image");
+
+    let mut h_m = m.lock().await;
+    h_m.insert(file_name.to_string(), String::from("sucess"));
+
+    let elapsed_time = now.elapsed();
+    println!(
+        "Running save file {} took {} seconds.",
+        file_name,
+        elapsed_time.as_secs()
+    );
     Ok(())
 }
--- src/routes/uploads/mod.rs
+++ src/routes/uploads/mod.rs
@@ -7,8 +7,8 @@ use rocket::http::ContentType;
 use rocket::serde::json::{json, Value};
 use rocket::tokio::{self, sync::Mutex};
 use rocket::Data;
-use rocket_multipart_form_data::{ mime,
-    MultipartFormData, MultipartFormDataField, MultipartFormDataOptions,
+use rocket_multipart_form_data::{
+    mime, MultipartFormData, MultipartFormDataField, MultipartFormDataOptions,
 };
 
 use crate::internal::image as image_clasify;
@@ -18,11 +18,12 @@ const MAX_FILES: u8 = 10;
 #[post("/upload", data = "<data>")]
 pub async fn uploads_file(content_type: &ContentType, data: Data<'_>) -> Value {
     // let files_field = vec![MultipartFormDataField::file("file")].repeat(MAX_FILES as usize);
-    let files_field = iter::repeat(MultipartFormDataField::file("file").content_type(Some(mime::IMAGE_STAR)))
+    let options: MultipartFormDataOptions = MultipartFormDataOptions {
+        allowed_fields: iter::repeat(
+            MultipartFormDataField::file("file").content_type(Some(mime::IMAGE_STAR)),
+        )
         .take(MAX_FILES as usize)
-        .collect();
-    let options = MultipartFormDataOptions {
-        allowed_fields: files_field,
+        .collect(),
         ..MultipartFormDataOptions::default()
     };
 
@@ -36,15 +37,15 @@ pub async fn uploads_file(content_type: &ContentType, data: Data<'_>) -> Value {
         None => return json!({"error": "No file upload"}),
         Some(v) => v,
     };
-    
+
     let clasify_response: Arc<Mutex<HashMap<String, String>>> =
         Arc::new(Mutex::new(HashMap::new()));
+
     let mut wait_files: Vec<_> = vec![];
     for _file in files {
         let clone = Arc::clone(&clasify_response);
         let path = String::from(_file.path.to_str().unwrap());
-        let file_name = _file.file_name.as_ref().unwrap();
-        let file_name = String::from(file_name);
+        let file_name = String::from(_file.file_name.as_ref().unwrap());
 
         let worker = tokio::spawn(async move {
             image_clasify::process_image(&clone, &file_name[..], &path[..])
